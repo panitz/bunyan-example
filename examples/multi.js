ecc1d23e3f6e44f79635e30d586ceafc5ea092a7
Thorsten Panitz <th.panitz@googlemail.com>
2021-10-30 23:41:19 +0200

    Added multi example.

---
+++ examples/multi.js
@@ -0,0 +1,20 @@
+const bunyan = require('bunyan');
+log = bunyan.createLogger({
+    name: 'amon',
+    streams: [
+        {
+            level: 'info',
+            stream: process.stdout,
+        },
+        {
+            level: 'error',
+            path: 'multi.log'
+        }
+    ]
+});
+
+
+log.debug('hi nobody on debug');
+log.info('hi stdout on info');
+log.error('hi both on error');
+log.fatal('hi both on fatal');
